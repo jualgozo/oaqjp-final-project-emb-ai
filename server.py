595c24e00b5dbcdca4c76ef78a2045cc2252fcab
jualgozo <jualzo@hotmail.com>
2025-08-07 01:55:19 -0400

    Server Deploy

--- server.py
+++ server.py
@@ -1,36 +1,30 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
-
 from flask import Flask, render_template, request
-from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
-
-#Initiate the flask app : TODO
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_emotion():
-    # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
+    
+    if not text_to_analyze or not text_to_analyze.strip():
+        return "Invalid text! Please try again!"
 
-    # Pass the text to the emotion_detector function and store the response
     response = emotion_detector(text_to_analyze)
 
-    # Extract the label and score from the response
-    label = response['label']
-    score = response['score']
+    if response.get('dominant_emotion') is None:
+        return "Invalid text! Please try again!"
 
-    # Check if the label is None, indicating an error or invalid input
-    if label is None:
-        return "Invalid input! Try again."
-    else:
-        # Return a formatted string with the sentiment label and score
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
 
+    return (f"For the given statement, the system response is 'anger': {anger}, "
+            f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and "
+            f"'sadness': {sadness}. The dominant emotion is {dominant_emotion}.")
 
 @app.route("/")
 def render_index_page():
