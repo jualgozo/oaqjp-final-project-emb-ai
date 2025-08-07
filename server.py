55f0994fc2279612f72b8d8d78a17d08680deca2
jualgozo <jualzo@hotmail.com>
2025-08-07 01:37:41 -0400

    Server

---
+++ server.py
@@ -0,0 +1,40 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask pramework package : TODO
+# Import the sentiment_analyzer function from the package created: TODO
+
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+#Initiate the flask app : TODO
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def sent_emotion():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extract the label and score from the response
+    label = response['label']
+    score = response['score']
+
+    # Check if the label is None, indicating an error or invalid input
+    if label is None:
+        return "Invalid input! Try again."
+    else:
+        # Return a formatted string with the sentiment label and score
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
